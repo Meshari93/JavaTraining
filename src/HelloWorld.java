670a58bfdddcc80170a077b6346dffe3923defe9
Meshari <almeshari93@gmail.com>
2019-08-24 04:29:46 +0300

    Long Primitive

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+        public HelloWorld(){
+            System.out.println("Hello Meshari");
+            int myFirstNumber = 5;
+            int mySecondNumber = 12;
+            int myThirdNumber = 6;
+            int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+            System.out.println("My Total is :" + myTotal);
+            System.out.println("My First Number : " + myFirstNumber);
+        }
+
+}

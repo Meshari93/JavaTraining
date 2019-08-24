9fa51f1e29dde3831e07da122f3ad1656f6c6007
Meshari <almeshari93@gmail.com>
2019-08-25 02:15:31 +0300

    Floating Point

--- src/ByteShortInt.java
+++ src/ByteShortInt.java
@@ -19,11 +19,38 @@ public class ByteShortInt {
         System.out.println("Maximum Value of Short is : " + myMaxShortValue);
 
         long myLongValue = 100L;
-        
+
         long myMinLongValue = Long.MIN_VALUE;
         long myMaxLongValue = Long.MAX_VALUE;
         System.out.println("Minimum Value of Long is : " + myMinLongValue);
         System.out.println("Maximum Value of Long is : " + myMaxLongValue);
+
+        short bigShortLiteralValue = 32767;
+
+        int myTotal = (myMinByteValue / 2);
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+        short myNewShortValue = (short) (myMinShortValue / 2);
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Minimum Value of Float is : " + myMinFloatValue);
+        System.out.println("Maximum Value of Float is : " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Minimum Value of Double is : " + myMinDoubleValue);
+        System.out.println("Maximum Value of Double is : " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+
+
+
     }
 
 }

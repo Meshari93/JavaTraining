3e2969b540069dec8c2fc7f8b6f735470a86723a
Meshari <almeshari93@gmail.com>
2019-08-24 05:03:21 +0300

    Casting In Java

--- src/ByteShortInt.java
+++ src/ByteShortInt.java
@@ -3,8 +3,27 @@ public class ByteShortInt {
         int myValue = 1000;
         int myMinIntValue = Integer.MIN_VALUE;
         int myMaxIntValue = Integer.MAX_VALUE;
-        System.out.println("My Minimum Value of Integer is : " + myMinIntValue);
-        System.out.println("My maximum Value of Integer is : " + myMaxIntValue);
+        System.out.println("Minimum Value of Integer is : " + myMinIntValue);
+        System.out.println("Maximum Value of Integer is : " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + myMaxIntValue + 1);
+        System.out.println("Busted MIN value = " + myMinIntValue + 1);
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Minimum Value of Byte is : " + myMinByteValue);
+        System.out.println("Maximum Value of Byte is : " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Minimum Value of Short is : " + myMinShortValue);
+        System.out.println("Maximum Value of Short is : " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Minimum Value of Long is : " + myMinLongValue);
+        System.out.println("Maximum Value of Long is : " + myMaxLongValue);
     }
 
 }
--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,16 @@
 public class Hello {
     public static void  main(String[] args){
-     HelloWorld helloWorld = new HelloWorld();
 
+        /**
+         * Hello World
+         * 1- Variables
+         */
+
+//     HelloWorld helloWorld = new HelloWorld();
+
+/**
+ * Primitive Type
+ */
      ByteShortInt byteShortInt = new ByteShortInt();
      }
 }
